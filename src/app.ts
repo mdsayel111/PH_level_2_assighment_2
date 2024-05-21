775c7640c8fcd5bf2d6253c455202fed2c73a154
sayel <mdsayel111@gmail.com>
2024-05-21 17:18:03 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { productRouter } from './modules/product/product.route';
 const app = express();
 
@@ -12,11 +12,22 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.all("*", (req: Request, res: Response)=> {
-res.status(404).send({
-    success: false,
-    message: "No route found!",
+// not found route handler
+app.all("*", (req: Request, res: Response) => {
+    res.status(404).send({
+        success: false,
+        message: "No route found!",
+    })
 })
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.log("err")
+    res.status(500).send({
+        success: false,
+        message: error.message,
+        error
+    })
 })
 
 export default app;
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import productService from "./product.service"
 
-export const creatProduct = async (req: Request, res: Response) => {
+export const creatProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const productData = req.body
         const result = await productService.creatProduct(productData)
@@ -11,21 +11,16 @@ export const creatProduct = async (req: Request, res: Response) => {
             data: result
         })
     } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: error.message,
-            error
-        })
+        next(error)
     }
 }
 
-export const getAllProduct = async (req: Request, res: Response) => {
+export const getAllProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const query = req.query
         const queryLength = Object.keys(query).length
         if (queryLength > 0) {
             const result = await productService.searchProduct(query as Record<string, string>)
-            console.log(result)
             return res.status(200).send({
                 success: true,
                 message: "Product fetched successfully!",
@@ -39,15 +34,11 @@ export const getAllProduct = async (req: Request, res: Response) => {
             data: result
         })
     } catch (error: any) {
-        res.status(500).send({
-            success: false,
-            message: error.message,
-            error
-        })
+        next(error)
     }
 }
 
-export const getSingleProduct = async (req: Request, res: Response) => {
+export const getSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { productId } = req.params
         const result = await productService.getSingleProduct(productId)
@@ -57,15 +48,11 @@ export const getSingleProduct = async (req: Request, res: Response) => {
             data: result
         })
     } catch (error: any) {
-        res.status(500).send({
-            success: false,
-            message: error.message,
-            error
-        })
+        next(error)
     }
 }
 
-export const updateSingleProduct = async (req: Request, res: Response) => {
+export const updateSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { productId } = req.params
         const productData = req.body
@@ -76,15 +63,11 @@ export const updateSingleProduct = async (req: Request, res: Response) => {
             data: result
         })
     } catch (error: any) {
-        res.status(500).send({
-            success: false,
-            message: error.message,
-            error
-        })
+        next(error)
     }
 }
 
-export const deleteSingleProduct = async (req: Request, res: Response) => {
+export const deleteSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { productId } = req.params
         await productService.deleteSingleProduct(productId)
@@ -94,10 +77,6 @@ export const deleteSingleProduct = async (req: Request, res: Response) => {
             data: null
         })
     } catch (error: any) {
-        res.status(500).send({
-            success: false,
-            message: error.message,
-            error
-        })
+        next(error)
     }
 }
